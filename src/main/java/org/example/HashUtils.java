b7e95a5e8c21b81fc2d9d0770e5e81fb480c35cd
katrinDev <polozkovak8@gmail.com>
2023-05-03 00:48:00 +0300

    fix the hash-function

--- src/main/java/org/example/HashUtils.java
+++ src/main/java/org/example/HashUtils.java
@@ -7,11 +7,11 @@ public class HashUtils {
         for (int i = 0; i < key.length(); i++) {
             hash += key.charAt(i);
             hash += hash << 10;
-            hash ^= hash >> 6;
+            hash ^= hash >>> 6;
         }
 
         hash += hash << 3;
-        hash ^= hash >> 11;
+        hash ^= hash >>> 11;
         hash += hash << 15;
 
         return Integer.toHexString(hash);
